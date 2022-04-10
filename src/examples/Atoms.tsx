8165f0e70f41b3726a7300cbfea8d1765a142af3
Hamza MABROUK <mabrouk.hamza@gmail.com>
2022-04-10 18:32:43 +0000

    recoil first commit

---
+++ src/examples/Atoms.tsx
@@ -0,0 +1,42 @@
+import {useState} from 'react'
+import {atom, useRecoilState, useRecoilValue} from 'recoil'
+
+const darkModeAtom = atom({
+    key: 'darkMode',
+    default: false,
+})
+
+const DarkModeSwitch = () => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeAtom)
+    return (
+        <input
+            type="checkbox"
+            checked={darkMode}
+            onChange={(event) => {
+                setDarkMode(event.currentTarget.checked)
+            }}
+        />
+    )
+}
+
+const Button = () => {
+    const darkMode = useRecoilValue(darkModeAtom)
+    return (
+        <button style={{backgroundColor: darkMode ? 'black' : 'white', color: darkMode ? 'white' : 'black'}}>
+            UI button
+        </button>
+    )
+}
+
+export const Atoms = () => {
+    return (
+        <div>
+            <div>
+                <DarkModeSwitch />
+            </div>
+            <div>
+                <Button />
+            </div>
+        </div>
+    )
+}
